16687427f5024cd72bb30fd8c40f76f7fff809d8
wuguicheng <xxx>
2022-01-09 19:57:03 +0800

    修改

--- demo/index.js
+++ demo/index.js
@@ -7,26 +7,6 @@ import {
 } from 'react-native';
 import App from './app';
 
-class HelloWorld extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.hello}>Hello, World</Text>
-      </View>
-    );
-  }
-}
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center'
-  },
-  hello: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  }
-});
 
 AppRegistry.registerComponent(
   'MyReactNativeApp',
